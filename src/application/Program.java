76d82b5f1829823a8576db0d028db17f95f537d8
GianluccaCampana <gianluccacampana19@gmail.com>
2021-03-29 23:16:36 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,15 +13,47 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Book");
-		
-		//System.out.println(obj);
+		Scanner scan = new Scanner(System.in);
 
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		
-		
-		SellerDao sellerDao =  DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== Test 1: seller findById ===");
+		Seller seller = sellerDao.findById(16);
 		System.out.println(seller);
+		
+		System.out.println("\n=== Test 2: seller findByIdDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== Test 3: seller findAll ===");
+		
+		 list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== Test 4: seller insert ===");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update ===");
+		
+		seller = sellerDao.findById(20);
+		seller.setName("Ronaldo");		
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== Test 6: seller update ===");
+		System.out.println("Enter id for delete test");
+		int id = scan.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
